55d21018e490f63e40ba68d9414b2bbfb021efa7
Dieguidev <diegogaraycullas@gmail.com>
2025-01-05 22:42:28 -0500

    first commit

---
+++ src/test/java/dieguidev/devtree/DevtreeApplicationTests.java
@@ -0,0 +1,13 @@
+package dieguidev.devtree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevtreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
